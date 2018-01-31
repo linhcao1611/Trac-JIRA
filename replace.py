df487312f03c5cecdd6a21e368889f2d01470d61
Linh Cao <linh@Linhs-MacBook-Pro.local>
2018-01-31 14:39:28 -0800

    don't need to display as thumbnail

--- replace.py
+++ replace.py
@@ -7,5 +7,5 @@ textToReplace = '!'
 
 for line in fileinput.FileInput(inputFile, inplace=1):
     line=line.replace('[[Image(','!https://s3-us-west-2.amazonaws.com/eplan-jira/attachments/')
-    line=line.replace(')]]','|thumbnail! ')
+    line=line.replace(')]]','! ')
     print line
