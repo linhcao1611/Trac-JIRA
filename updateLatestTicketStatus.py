9f7639a813d43d7ae2b0e7e613856606aa159156
Linh Cao <linh@Linhs-MacBook-Pro.local>
2018-02-06 17:46:11 -0800

    update time format

--- updateLatestTicketStatus.py
+++ updateLatestTicketStatus.py
@@ -8,8 +8,8 @@ c = conn.cursor()
 ticketResult = list(c.execute("select * from ticket"))
 
 for row in ticketResult:
-    time = str(datefmt.to_datetime(row[2]))
-    changetime = str(datefmt.to_datetime(row[3]))
+    time = str(datefmt.format_datetime(datefmt.to_datetime(row[2]), tzinfo=None, locale=None))
+    changetime = str(datefmt.format_datetime(datefmt.to_datetime(row[3]), tzinfo=None, locale=None))
     trac_id = str(row[0])
     print trac_id + ' - ' + time + ' - ' + changetime
     c.execute("update ticket set time=?, changetime=? where id=?", (time, changetime,trac_id))
